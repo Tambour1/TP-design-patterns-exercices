f5bccea3560bd13e38826651cb62cb966c0cd134
Victor Georges <victorgeorges54@gmail.com>
2025-03-10 17:09:24 +0100

    ✨ Ajout exercice Singleton

--- singleton/app/Config.php
+++ singleton/app/Config.php
@@ -3,3 +3,33 @@
 # TODO: Créer une classe Config en Singleton
 
 namespace App;
+
+final class Config
+{
+    private $settings =[];
+    private static $instance;
+
+    private function __construct()
+    {
+        $path = realpath(__DIR__ . '/../config/config.php');
+        $this->settings = include $path;
+    }
+
+    public static function getInstance(): Config
+    {
+        if (!self::$instance) {
+            self::$instance = new self();
+        }
+
+        return self::$instance;
+    }
+
+    public function get(string $key) {
+        if (!isset($this->settings[$key])) {
+            return null;
+        }
+        return $this->settings[$key];
+    }
+
+    
+}
--- singleton/public/index.php
+++ singleton/public/index.php
@@ -1,7 +1,17 @@
 <?php
 require('../vendor/autoload.php');
 
+use App\Config;
+use PHPUnit\Event\Runtime\PHP;
 
 # TODO: Récuperer une instance de Config
 # Afficher une valeur contenu dans config.php
 # Récupérer une seconde instance de Config et vérifié que les deux instances sont identiques
+
+$config1 = Config::getInstance();
+echo $config1->get('db')['host'];
+$config2 = Config::getInstance();
+
+echo PHP_EOL;
+
+var_dump($config1 === $config2); 
